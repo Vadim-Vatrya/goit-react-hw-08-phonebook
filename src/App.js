3bd92fdb821e5375a7d865cb3e4e31a32931d93d
Vadim-Vatrya <VatryaVV@gmail.com>
2021-04-10 15:04:39 +0300

    Copy files HW 7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getLoading } from './redux/contacts/contact-selectors';
+import {fetchContacts} from './redux/contacts/contact-operations';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Container from './components/Container';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+import Loader from './components/Loader';
+
+const App = () => {
+    // const contacts = useSelector(getContacts);
+    const loading = useSelector(getLoading);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
+
+    return (
+    <Container>
+        <h1>Phonebook</h1>
+        <ContactForm />
+
+        <h2>Contacts</h2>
+        {/* {loading && <Loader />} */}
+        <Loader
+          type="Puff"
+          color="#000"
+          height={70}
+          width={70}
+          visible={loading}
+        />
+        {/* {contacts.length > 0 
+        ? ( */}
+        <>
+         <Filter />
+         <ContactList />
+        </>
+        {/* )
+        : (<p>No contacts</p>)} */}
+       
+    </Container>
+    )
+};
 
 export default App;
