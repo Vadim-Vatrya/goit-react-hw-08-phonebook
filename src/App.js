92ad1c29f5e827d2b8cf3ed2bfc2c73418b76b7b
Vadim-Vatrya <VatryaVV@gmail.com>
2021-04-10 21:36:48 +0300

    start route

--- src/App.js
+++ src/App.js
@@ -8,42 +8,57 @@ import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList';
 import Loader from './components/Loader';
+import { Switch } from '@material-ui/core';
+import { Router } from '@material-ui/icons';
 
-const App = () => {
-    // const contacts = useSelector(getContacts);
-    const loading = useSelector(getLoading);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(fetchContacts());
-    }, [dispatch]);
-
-
-    return (
+ 
+const App =() => {
     <Container>
-        <h1>Phonebook</h1>
-        <ContactForm />
-
-        <h2>Contacts</h2>
-        {/* {loading && <Loader />} */}
-        <Loader
-          type="Puff"
-          color="#000"
-          height={70}
-          width={70}
-          visible={loading}
-        />
-        {/* {contacts.length > 0 
-        ? ( */}
-        <>
-         <Filter />
-         <ContactList />
-        </>
-        {/* )
-        : (<p>No contacts</p>)} */}
-       
+        <AppBar />
+        <Switch>
+            <Router exact path='/' component={HomeView} />
+            <Router path='/contacts' component ={ContactsView} />
+            <Router path='/register' component={RegisterView} />
+            <Router path='/login' component={LoginPage} />
+        </Switch>
     </Container>
-    )
-};
+}
 
 export default App;
+
+// const App = () => {
+//     // const contacts = useSelector(getContacts);
+//     const loading = useSelector(getLoading);
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         dispatch(fetchContacts());
+//     }, [dispatch]);
+
+
+//     return (
+//     <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm />
+
+//         <h2>Contacts</h2>
+//         {/* {loading && <Loader />} */}
+//         <Loader
+//           type="Puff"
+//           color="#000"
+//           height={70}
+//           width={70}
+//           visible={loading}
+//         />
+//         {/* {contacts.length > 0 
+//         ? ( */}
+//         <>
+//          <Filter />
+//          <ContactList />
+//         </>
+//         {/* )
+//         : (<p>No contacts</p>)} */}
+       
+//     </Container>
+//     )
+// };
