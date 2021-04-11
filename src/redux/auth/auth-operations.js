52530bb8c4981bb00e1009379b01fb9e94017f41
Vadim-Vatrya <VatryaVV@gmail.com>
2021-04-11 11:57:35 +0300

    add auth-reducers

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import authActions from './auth-actions';
+
+axios.defaults.baseURL = 'https://goit-phonebook-api.herokuapp.com';
+
+
---
+++ src/redux/auth/auth-reducers.js
@@ -0,0 +1,27 @@
+import {combineReducers} from 'redux';
+import {createReducer} from  '@reduxjs/toolkit';
+import authActions from './auth-actions';
+
+const initialUserState = { name: null, email: null };
+
+const user = createReducer(initialUserState, {} );
+
+
+const token = createReducer(null, {});
+
+
+const setError = (_, { payload }) => payload;
+
+const error = createReducer(null, {});
+
+
+const isAuthenticated = createReducer(false, {});
+
+
+export default combineReducers({
+  user,
+  isAuthenticated,
+  token,
+  error,
+});
+
